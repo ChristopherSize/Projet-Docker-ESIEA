f67ed630142ea9b7eba42b7faa13a73ff19c678b
MVOGO ZE Aurel Daniel <mvogoze@et.esiea.fr>
2024-12-22 19:28:19 +0100

    feat: Modification permettant de satisfaire la cahier des charges

--- projet_docker_esiea/src/app/app.component.ts
+++ projet_docker_esiea/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
+  edit :boolean=false;
+  id :any;
   public getJsonValue: any;
   public postJsonValue: any;
   name :string = "";
@@ -18,13 +21,12 @@ export class AppComponent {
   title = 'projet_docker_esiea';
   ngOnInit(): void {
     this.getMethod();
-    this.postMethod();
   }
 
   public getMethod(){
     //Remplacé ce lien par le entry point get de pièrre
     //Que fait la method subscribe
-    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe((data) =>{
+    this.http.get('http://localhost:3000/mangas').subscribe((data) =>{
       console.log(data);
       this.getJsonValue = data;
     });
@@ -33,14 +35,35 @@ export class AppComponent {
   public postMethod(){
     //On ajoute les crochets pour indiquer les données qu'on souhaite envoyé à notre base de données
     let body = {
-      id:'',
-      name: ''+this.name,
-      type: ''+this.type,
-      note: ''+this.note,
+      "name": ''+this.name,
+      "type": ''+this.type,
+      "note": ''+this.note,
     };
-    this.http.post('https://jsonplaceholder.typicode.com/posts',body).subscribe((data) =>{
+    this.http.post('http://localhost:3000/register',body).subscribe((data) =>{
       console.log(data);
       this.postJsonValue = data;
     });
+  }
+  public deleteMethod(id:any){
+    this.http.delete('http://localhost:3000/delete/'+id).subscribe((data) =>{
+      console.log("delete");
+    });
+  }
+
+  public editMethod(){
+    //On ajoute les crochets pour indiquer les données qu'on souhaite envoyé à notre base de données
+    let body = {
+      "name": ''+this.name,
+      "type": ''+this.type,
+      "note": ''+this.note,
+    };
+    this.http.put('http://localhost:3000/edit/'+this.id,body).subscribe((data) =>{
+      console.log(data);
+    });
+    this.edit = !this.edit;
+  }
+  public getId(id:any){
+    this.id = id;
+    this.edit = !this.edit;
   }
 }
